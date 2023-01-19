3ac4ffa7482ca16d89e6b955eb7206c2c582089f
GladysKigen <gladys.kigen@student.moringa.com>
2023-01-19 15:47:03 +0300

    updated

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inTheCart, setInTheCart] =useState(false);
+   //(inTheCart? "inTheCat": "")
+
+  function handleInCart(){
+    setInTheCart(inTheCart => !inTheCart)
+  }
+
   return (
-    <li className="">
+    <li className="cart">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = {handleInCart}>{inTheCart? "Add to Cart": "Remove from Cart"}</button>
     </li>
   );
 }
